efabbff584e38a46ade3c720146d7a010fd38bc6
Brian Palmer <bpalmer710@gmail.com>
2020-10-07 01:03:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,11 @@
 def oxford_comma(array)
   if array.size > 2 
-    last_name = array.pop
+    last_fruit = array.pop
+    the_fruit << ", and #{last_fruit}"
     
+  elsif array.size === 2 
+    return array.join (" and ")
+  else
+    return array.join
+end
 end
