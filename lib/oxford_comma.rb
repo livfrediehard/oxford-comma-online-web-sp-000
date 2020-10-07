d155f52ab13592694549f051cc2d4aab58c33f18
Brian Palmer <bpalmer710@gmail.com>
2020-10-07 01:09:14 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size > 2 
+    last_person = array.pop
+    my_people = array.join(", ")
+    my_people << ", and #{last_person}"
+  elsif array.size === 2
+    return array.join(" and ")
+  else
+    return array.join
+  end
 end
